8893a2a96b3161fac04e93177fd149a4cba2aa2f
Chloe Jenner <crjdowning@hotmail.com>
2023-04-19 12:10:12 +0100

    dictionary visuals set up

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity, Image } from 'react-native';
+import {images} from "./assets/images"
 
 export default function App() {
+const image = images.sky
+
+const [newWord, setNewWord] = useState('')
+const searchWord= (enteredWord)=> {
+  setNewWord(enteredWord)
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground
+      style={{flex: 1}}
+      resizeMode="cover"
+      source={image}>
+      <View style={{flex:0.8}}>
+        <View style={{justifyContent: 'center', alignItems: "center"}}>
+          <TextInput 
+          style={styles.inputBox}
+          placeholder='search a word'
+          placeholderTextColor={'grey'}
+          textAlign="center"
+          clearButtonMode='always'
+          onChangeText={searchWord}
+          value={newWord}
+          >
+          </TextInput>
+        </View>
+        <View style = {{
+          flexDirection : 'row',
+          justifyContent: 'space-between',
+          marginTop: 20,
+          marginBottom: 20
+        }
+        }>
+          <TouchableOpacity style={styles.buttons}>
+            <Text style={styles.buttonText}>Go !</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttons}>
+            <Text style={styles.buttonText}>Clear</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require('./assets/Speaker.png')} style={styles.speakerButton}/>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text>{newWord}</Text>
+        </View>
+      </View>
+      
       <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -14,7 +62,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  inputBox:{
+    width: "80%",
+    height: 50,
+    borderWidth:5,
+    borderColor: "blue",
+    marginTop:100,
+    fontSize:25
+  },
+  speakerButton: {
+    height:40,
+    width: 50
+  },
+  buttons: {
+    borderRadius: 30,
+    borderWidth: 1,    
+    borderColor: 'black',
+    width: '20%'
+
+  },
+  buttonText: {
+    fontSize: 25,
+    alignSelf: 'center',
+    marginTop:5
+  }
 });
---
+++ assets/images.js
@@ -0,0 +1,4 @@
+export const images = {
+    sky:{uri: 'https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?w=1380&t=st=1681900213~exp=1681900813~hmac=47dd2993424882565c905493f864fe03cf207d7e18034389db2937418685f8a1'},
+    speaker: { uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Speaker_Icon.svg/2048px-Speaker_Icon.svg.png'}
+}
